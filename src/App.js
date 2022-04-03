8f629d8539afad806d5bc52928cdb7490690bf82
RayanBallout <rayanballout14@gmail.com>
2022-04-03 21:16:08 +0300

    Resetting and adding react dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 function App() {
-	return <div className="App">Hellooo</div>
+	return <div className="container">Hellooo</div>
 }
 
 export default App
